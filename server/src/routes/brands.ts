ab0a3f203c16530363349547a53e104b5c81cb7c
shamimgazi8 <shamimshamim.g47@hotmail.com>
2025-04-17 16:33:42 +0600

    server brnad created protected

--- server/src/routes/brands.ts
+++ server/src/routes/brands.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { getAllBrands, createBrand } from "../controllers/brand.controller";
+import authMiddleware from "../middleware/authMiddleware";
 const router = Router();
 
 router.get("/", getAllBrands);
-router.post("/", createBrand as any);
+router.post("/", authMiddleware, createBrand as any);
 
 export default router;
